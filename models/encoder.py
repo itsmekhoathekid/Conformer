6989bf5d8acd71effc2243bfb235b4acb6499da9
Khoa <22520675@gm.uit.edu.vn>
2025-10-25 10:25:45 +0700

    add another front end cnn

--- models/encoder.py
+++ models/encoder.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from .modules import Linear, Conv2dSubampling, FeedForwardModule, ConvolutionalModule, ResidualConnection, MultiConvolutionalGatingMLP
+from .modules import Linear, Conv2dSubampling, FeedForwardModule, ConvolutionalModule, ResidualConnection, MultiConvolutionalGatingMLP, ConvolutionFrontEnd, get_mask_from_lens
 from .attention import MultiHeadedSelfAttentionModule
 
 class ConformerBlock(nn.Module):
@@ -41,14 +41,37 @@ class ConformerBlock(nn.Module):
 class ConformerEncoder(nn.Module):
     def __init__(self, config):
         super(ConformerEncoder, self).__init__()
-        self.subsampling = Conv2dSubampling(
-            in_channels = config["in_channels"],
-            out_channels = config["encoder_dim"],
-        )
-        self.input_projection = nn.Sequential(
-            Linear(config["encoder_dim"] * (((config["input_dim"] - 1) // 2 - 1) // 2), config["encoder_dim"]),
-            nn.Dropout(p=config["dropout_rate"]),
-        )
+        self.sampling_type = config['subsampling_type']
+        if config['subsampling_type'] == 'conv2d':
+            self.subsampling = Conv2dSubampling(
+                in_channels = config["in_channels"],
+                out_channels = config["encoder_dim"],
+            )
+            self.input_projection = nn.Sequential(
+                Linear(config["encoder_dim"] * (((config["input_dim"] - 1) // 2 - 1) // 2), config["encoder_dim"]),
+                nn.Dropout(p=config["dropout_rate"]),
+            )
+        elif config['subsampling_type'] == 'conv_frontend':
+            self.subsampling = ConvolutionFrontEnd(
+                in_channels=1,
+                num_blocks=config['subsampling']['num_blocks'],
+                num_layers_per_block=config['subsampling']['num_layers_per_block'],
+                out_channels=config['subsampling']['out_channels'],
+                kernel_sizes=config['subsampling']['kernel_sizes'],
+                strides=config['subsampling']['strides'],
+                residuals=config['subsampling']['residuals'],
+                activation=nn.ReLU,        
+                norm=nn.BatchNorm2d,            
+                dropout=0.1,
+            )
+            self.input_projection = nn.Sequential(
+                Linear(config['subsampling']['input_projection'], config["encoder_dim"]),
+                nn.Dropout(p=config["dropout_rate"]),
+            )
+        else:
+            raise ValueError(f"Unsupported subsampling type: {config['subsampling_type']}")
+
+        
         
        
         
@@ -68,7 +91,15 @@ class ConformerEncoder(nn.Module):
         
 
     def forward(self, x, x_length, training=True):
-        x, x_length = self.subsampling(x, x_length)  # (batch, time', dim)
+        if self.sampling_type == 'conv_frontend':
+            x = x.unsqueeze(1)  # [batch, channels, time, features]
+            x_mask = get_mask_from_lens(x_length, x.size(2))  # [batch, 1, time]
+            x, mask = self.subsampling(x, x_mask)  # [batch, channels, time, features]
+            x = x.transpose(1, 2).contiguous()   # batch, time, channels, features
+            x = x.reshape(x.shape[0], x.shape[1], -1) # [batch, time, C * features]
+            x_length = mask.sum(-1)
+        elif self.sampling_type == 'conv2d':
+            x, x_length = self.subsampling(x, x_length)  # (batch, time', dim)
         x = self.input_projection(x)  # (batch, time', dim)
 
 
